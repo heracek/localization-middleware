ca47a36da5b7e1e4d17629c80ae1230052f4af7d
Tomáš Horáček <tomas.horacek@gmail.com>
2014-11-28 12:21:59 +0100

    enabled running `rake task` on ruby 2.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,6 @@ require 'rubygems'
 require 'test/unit'
 require 'capybara'
 require 'capybara/dsl'
-if RUBY_VERSION >= '1.9.0'
-  require "debugger"
-else
-  require 'ruby-debug'
-end 
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
